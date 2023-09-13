cda7da5521db2063721e42d586c4b6cd5532131a
juseitor <142334949+juseitor@users.noreply.github.com>
2023-09-13 20:31:53 -0300

    Add files via upload

--- guia4.hs
+++ guia4.hs
@@ -21,8 +21,7 @@ esDivisible x y | x == y = True
 
 sumaImpares :: Integer -> Integer
 sumaImpares x | x == 1 = 1
-              | mod x 2 == 0 = sumaImpares (x-1) + x + 1
-              | otherwise = sumaImpares (x-2) + x
+              | otherwise = sumaImpares (2*x-1)
 
 --5)
 
@@ -49,3 +48,51 @@ todosDigitosIguales :: Integer -> Bool
 todosDigitosIguales n | n < 10 = True
                       | otherwise = (ultimoDigito n == ultimoDigito(sacarUltimo n)) && todosDigitosIguales (sacarUltimo n)
     
+--8)
+
+cantDigitos :: Integer -> Integer
+cantDigitos n | (n >= 0 && n < 10) = 1 
+              | otherwise = 1 + cantDigitos (div n 10)
+
+iesimoDigito :: Integer -> Integer -> Integer
+iesimoDigito n i | i == cantDigitos(n) = mod n 10
+                 | otherwise = iesimoDigito (div n 10) i
+
+--9)
+
+primerDigito :: Integer -> Integer
+primerDigito n | cantDigitos n == 1 = n
+               | otherwise = primerDigito (div n 10) 
+
+sacarPrimerDigito :: Integer -> Integer
+sacarPrimerDigito n = n - (primerDigito n * conversion n)
+
+conversion :: Integer -> Integer
+conversion n | n < 10 && n > 0 = 1
+             | otherwise = 10 * conversion (div n 10)
+
+esCapicua :: Integer -> Bool
+esCapicua n | cantDigitos n == 1 = True
+            | (cantDigitos n == 2 && primerDigito n == ultimoDigito n) = True   
+            | cantDigitos n >= 3 = esCapicua (div (sacarPrimerDigito n) 10)
+            | otherwise = False
+
+--10)
+
+--f1
+
+f1 :: Integer -> Integer
+f1 n | n == 0 = 1
+     | otherwise = 2^n + f1 (n-1)
+
+--f2
+
+f2 :: Integer -> Float -> Float
+f2 n q | n == 1 = q 
+       | otherwise = q^n + f2 (n-1) q
+
+--f3
+
+f3 :: Integer -> Float -> Float
+f3 n q | n == 0 = 1
+       | otherwise = q^(2*n) + f3 (2*n-1) q
